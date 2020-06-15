fc7786baadd641778c449824a15c281e6d6a4eff
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-06-15 21:07:03 +0200

    Initialize Xcode project

---
+++ Life/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Life
+//
+//  Created by Nityananda Zbil on 15.06.20.
+//  Copyright © 2020 Nityananda Zbil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
