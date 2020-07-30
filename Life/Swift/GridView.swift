0de0156afda5126b3044b9a77970c7d0079b900a
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-07-30 13:19:58 +0200

    Extract DrawingVisitor

--- Life/Swift/GridView.swift
+++ Life/Swift/GridView.swift
@@ -8,46 +8,6 @@
 
 import Cocoa
 
-class DrawingVisitor: NSObject {
-    var view: NSView
-    
-    init(view: NSView) {
-        self.view = view
-    }
-}
-
-extension DrawingVisitor: GridVisitor {
-    func visitDenizen(_ denizen: Any!,
-                      onGrid grid: Any!,
-                      ofDimension n: Int,
-                      atX x: Int,
-                      y: Int) -> Any! {
-        let denizen = denizen as! Cell
-        let (n, x, y) = (CGFloat(n), CGFloat(x), CGFloat(y))
-        
-        let beginningHorizontal = x / n
-        let beginningVertical = y / n
-        let horizontalExtent = 1.0 / n
-        let verticalExtent = 1.0 / n
-        
-        let boundsSize = view.bounds.size
-        let cellRect = CGRect(x: beginningHorizontal * boundsSize.width,
-                              y: beginningVertical * boundsSize.height,
-                              width: horizontalExtent * boundsSize.width,
-                              height: verticalExtent * boundsSize.height)
-        
-        let path = NSBezierPath(rect: cellRect)
-        NSColor(calibratedWhite: CGFloat(denizen.population()),
-                alpha: 1.0).set()
-        path.stroke()
-        NSColor(calibratedWhite: 1.0 - CGFloat(denizen.population()),
-                alpha: 1.0).set()
-        path.fill()
-        
-        return denizen
-    }
-}
-
 class GridView: NSView {
 
     var currentGrid: Grid!
