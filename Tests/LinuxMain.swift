faaf8e22353a2ef8aaca87bd5890d1747f166085
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-07-31 14:32:00 +0200

    Revert package initialization

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import iamleeg_life_d8e60a635acaTests
-
-var tests = [XCTestCaseEntry]()
-tests += iamleeg_life_d8e60a635acaTests.allTests()
-XCTMain(tests)
--- Tests/iamleeg-life-d8e60a635acaTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(iamleeg_life_d8e60a635acaTests.allTests),
-    ]
-}
-#endif
