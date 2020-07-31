517d7d1512d600776c64f0cd59f836a15c86c80e
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-07-31 14:54:15 +0200

    Create new xcodeproj

---
+++ Life/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Life
+//
+//  Created by Nityananda Zbil on 31.07.20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Life/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Life
+//
+//  Created by Nityananda Zbil on 31.07.20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
